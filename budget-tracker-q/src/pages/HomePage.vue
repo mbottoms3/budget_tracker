09878c79adef4c8f4900d99b450f7909aadf11bb
mbottoms3 <micah.bottoms@coreware.com>
2024-02-10 13:02:00 -0600

    Added logic to handle data from components

--- budget-tracker-q/src/pages/HomePage.vue
+++ budget-tracker-q/src/pages/HomePage.vue
@@ -29,14 +29,29 @@
           :name="1"
           :done="step > 1"
         >
-          <monthly-expenses v-if="toggle" :isMobile="isMobile" />
+          <monthly-expenses
+            @monthly="handleMonthlyExpenses"
+            v-if="toggle"
+            :isMobile="isMobile"
+          />
           <custom-time-period v-else :isMobile="isMobile" />
         </q-step>
         <q-step title="Add Weekly Expenses" :name="2" :done="step > 2">
-          <weekly-expenses v-if="toggle" :isMobile="isMobile" />
+          <weekly-expenses
+            @weekly="handleWeeklyExpenses"
+            v-if="toggle"
+            :isMobile="isMobile"
+          />
         </q-step>
-        <q-step title="Add Income" :name="3" :done="step > 3">
-          <income-predictor v-if="toggle" :isMobile="isMobile" />
+        <q-step title="Review Expenses" :name="3" :done="step > 3">
+          <expenses-table :isMobile="isMobile" :expenses="allExpenses" />
+        </q-step>
+        <q-step title="Add Income" :name="4" :done="step > 4">
+          <income-predictor
+            @income="handleIncome"
+            v-if="toggle"
+            :isMobile="isMobile"
+          />
         </q-step>
       </q-stepper>
       <q-separator />
@@ -49,7 +64,7 @@
         />
         <q-btn v-else @click="budgeting = false" label="Cancel" no-caps />
         <q-btn
-          v-if="step < 3"
+          v-if="step < 4"
           no-caps
           label="Continue"
           @click="$refs.stepper.next()"
@@ -58,18 +73,21 @@
       </div>
     </div>
     <welcome-component v-else @start-budgeting="startBudgeting" />
+    <div>{{ weeklyExpenses }}</div>
+    <div>{{ monthlyExpenses }}</div>
+    <div>{{ income }}</div>
   </q-page>
 </template>
 
 <script>
-import { computed, ref } from "vue";
+import { computed, ref, watch } from "vue";
 import { useQuasar } from "quasar";
 
 import CustomTimePeriod from "src/components/CustomTimePeriod.vue";
 import WelcomeComponent from "../components/WelcomeComponent.vue";
 import MonthlyExpenses from "../components/MonthlyExpenses.vue";
 import WeeklyExpenses from "../components/WeeklyExpenses.vue";
-
+import ExpensesTable from "src/components/ExpensesTable.vue";
 import IncomePredictor from "../components/IncomePredictor.vue";
 
 export default {
@@ -80,6 +98,7 @@ export default {
     WelcomeComponent,
     CustomTimePeriod,
     IncomePredictor,
+    ExpensesTable,
   },
   setup() {
     const $q = useQuasar();
@@ -88,6 +107,7 @@ export default {
     });
     const budgeting = ref(false);
     const startBudgeting = () => {
+      step.value = 1;
       budgeting.value = true;
     };
     const toggle = ref(true);
@@ -95,12 +115,71 @@ export default {
     // Stepper Variables
     const step = ref(1);
 
+    // Monthly Expenses
+    const monthlyExpenses = ref([]);
+    const handleMonthlyExpenses = (newVal) => {
+      monthlyExpenses.value = newVal;
+    };
+    // Weekly Expenses
+    const weeklyExpenses = ref([]);
+    const handleWeeklyExpenses = (newVal) => {
+      weeklyExpenses.value = newVal;
+      console.log(weeklyExpenses.value);
+    };
+    // Income
+    const income = ref({
+      amount: "",
+      frequency: "",
+    });
+    const handleIncome = (newVal) => {
+      console.log(newVal);
+      income.value = newVal;
+    };
+
+    // Prepare expenses for table
+    const allExpenses = ref([]);
+    const expenseFormatter = () => {
+      // Process Monthly Expenses
+      monthlyExpenses.value.forEach((expense) => {
+        const formattedExpense = {
+          title: expense.title,
+          amount: expense.amount,
+          frequency: "monthly",
+        };
+        allExpenses.value.push(formattedExpense);
+      });
+
+      // Process Weekly Expenses
+      weeklyExpenses.value.forEach((expense) => {
+        const formattedExpense = {
+          title: expense.title,
+          amount: expense.amount * 4, // Multiply weekly amount by 4
+          frequency: "weekly",
+        };
+        allExpenses.value.push(formattedExpense);
+      });
+    };
+    watch(step, () => {
+      console.log(step.value);
+      if (step.value == 3) {
+        expenseFormatter();
+      }
+    });
+
     return {
       startBudgeting,
       budgeting,
       toggle,
       isMobile,
       step,
+      monthlyExpenses,
+      weeklyExpenses,
+      income,
+      handleMonthlyExpenses,
+      handleWeeklyExpenses,
+      handleIncome,
+      expenseFormatter,
+      allExpenses,
     };
   },
 };
